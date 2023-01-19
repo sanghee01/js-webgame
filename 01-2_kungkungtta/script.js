cd988548978d5f96f3d33f91a8a732071e6ef2bd
sanghee01 <sang6299@naver.com>
2023-01-19 18:01:56 +0900

    쿵쿵따

---
+++ 01-2_kungkungtta/script.js
@@ -0,0 +1,28 @@
+const $input = document.querySelector("input");
+const $button = document.querySelector("button");
+const $order = document.querySelector("#order");
+const $word = document.querySelector("#word");
+const number = Number(prompt("몇 명이 참가하나요?"));
+let word; // 제시어
+let newWord; // 현재 단어
+let order = Number($order.textContent);
+
+const onClickButton = () => {
+  if (newWord.length === 3 && (!word || word[word.length - 1] === newWord[0])) {
+    word = newWord;
+    $word.textContent = word;
+    order++;
+    if (order > number) order = 1;
+    $order.textContent = order;
+  } else {
+    alert("올바르게 입력하지 않았습니다.");
+  }
+  $input.value = "";
+  $input.focus();
+};
+const onInput = (e) => {
+  newWord = e.target.value;
+};
+
+$button.addEventListener("click", onClickButton);
+$input.addEventListener("input", onInput);
