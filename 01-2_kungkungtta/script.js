8ee8a058508e4f3d59b6900ad5d7396fb805b232
sanghee01 <sang6299@naver.com>
2023-01-19 18:07:33 +0900

    쿵쿵따 수정

--- 01-2_kungkungtta/script.js
+++ 01-2_kungkungtta/script.js
@@ -6,23 +6,27 @@ const number = Number(prompt("몇 명이 참가하나요?"));
 let word; // 제시어
 let newWord; // 현재 단어
 let order = Number($order.textContent);
+if (number) {
+  const onClickButton = () => {
+    if (
+      newWord.length === 3 &&
+      (!word || word[word.length - 1] === newWord[0])
+    ) {
+      word = newWord;
+      $word.textContent = word;
+      order++;
+      if (order > number) order = 1;
+      $order.textContent = order;
+    } else {
+      alert("올바르게 입력하지 않았습니다.");
+    }
+    $input.value = "";
+    $input.focus();
+  };
+  const onInput = (e) => {
+    newWord = e.target.value;
+  };
 
-const onClickButton = () => {
-  if (newWord.length === 3 && (!word || word[word.length - 1] === newWord[0])) {
-    word = newWord;
-    $word.textContent = word;
-    order++;
-    if (order > number) order = 1;
-    $order.textContent = order;
-  } else {
-    alert("올바르게 입력하지 않았습니다.");
-  }
-  $input.value = "";
-  $input.focus();
-};
-const onInput = (e) => {
-  newWord = e.target.value;
-};
-
-$button.addEventListener("click", onClickButton);
-$input.addEventListener("input", onInput);
+  $button.addEventListener("click", onClickButton);
+  $input.addEventListener("input", onInput);
+}
